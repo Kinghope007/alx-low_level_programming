893150348f8ed988ba72c272164288510279dcde
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-12 04:46:39 -0800

    z-a

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: Print Alphabets in reverse lowercase
- * Return: Always (Success)
+ * main - Print the lowercase alphabets
+ * Description: followed by new line
+ * Return: Always 0
  */
 
 int main(void)
 {
-	char letter;
+	int letter;
 
 	for (letter = 'z'; letter >= 'a'; letter--)
 		putchar(letter);
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
