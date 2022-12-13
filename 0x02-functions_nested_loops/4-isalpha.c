13bd4ac68ff7ccb2ac25d6a5f6ec6c65b4276447
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-13 13:56:04 -0800

    is c upper lowwer case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to check if c is a lettter, lower or uppercas
+ *  @c: is the int that will use for the argument of the function
+ *  Return: 0
+ */
+
+int _isaplha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
