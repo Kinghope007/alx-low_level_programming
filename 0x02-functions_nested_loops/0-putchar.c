7110e0197b86980b3b3666a9bdcd337e7d787fb5
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-13 13:06:11 -0800

    hope

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include "_putchar.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
  * main - print point 
@@ -8,13 +9,14 @@
 
 int main(void)
 {
-	char *hp = "_putchar";
-
-	while (*hp)
-	{
-		_putchar(*hp);
-		hp++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
