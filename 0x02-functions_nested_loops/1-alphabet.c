d23744aa910b47159bcee86b743bea2da2b0d7aa
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-13 13:17:00 -0800

    hope

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Entry poiny 
+ * Description: function to print a-z
+ * Return: 0
+ */
+
+void print_alphabets(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alpahbet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
