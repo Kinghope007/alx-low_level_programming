6e84881c7ec0bd8eeead695136209edd38f8ad17
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-16 06:19:37 -0800

    hope

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int i;
 
-	prntf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
 	while (i < 0)
 	{
 		putchar(i);
+		break;
 	}
 
-	prinf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
