407ac6f67c0d3297329740a69a6fda5f0f5bebd2
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-20 03:32:11 -0800

    hope

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 
 void print_to_98(int n)
 {
-	int i, j;
+	int i;
 
 	if (n <= 98)
 	{
@@ -28,7 +28,7 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				printf("%d, " j);
+				printf("%d, ", j);
 			else if (j == 98)
 				printf("%d\n", j);
 		}
