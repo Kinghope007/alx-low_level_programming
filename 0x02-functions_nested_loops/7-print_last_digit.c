f5c407c44c8d33b47dbbe396a40afc5ee44a3b85
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-20 03:15:46 -0800

    hope

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,18 @@
 
 int print_last_digit(int n)
 {
-	int a;
+	int last_digit;
+
 	if (n < 0)
-		n = -n;
-	a = n % 10;
-	if (a < 0)
-		a = -a;
-	_putchar(a + '0');
-	return (0);
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 }
