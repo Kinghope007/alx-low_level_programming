3b8ad259a59b3228fff414e7e86a109c1b758798
Kinghope007 <emmanuelhope39@gmail.com>
2022-12-23 04:57:41 -0800

    two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: pointer to char param
+ * @src: pointer to char param
+ * @n: int parameter
+ * Deacription: function that concatenates two strings.
+ * Return: *dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int m;
+	int i;
+
+	m = 0;
+
+	for (i = 0; i < 1000; i++)
+	{
+		if (destp[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m +i] = '\0';
+	return (dest);
+}
