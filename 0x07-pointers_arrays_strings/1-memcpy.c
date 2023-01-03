eb342f1512edeb3d38d1b6503b0b3e99190699e7
Kinghope007 <emmanuelhope39@gmail.com>
2023-01-02 18:01:26 -0800

    hope

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - fill memory with constant bytes
+ * @dest: memory area
+ * @src: memory area
+ * @n: bytes from memory to copy
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < n; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
