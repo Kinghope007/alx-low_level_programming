78bf3f21da2afe0a46d49c92753ccee230e0d057
Kinghope007 <emmanuelhope39@gmail.com>
2023-01-02 18:27:01 -0800

    crackme2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r = 0;
-	int i = n;
+	unsigned int a;
 
-	for (; r < n; r++)
+	for (a = 0; a < n; a++)
 	{
 		dest[r] = src[r];
-		n--;
 	}
 	return (dest);
 }
