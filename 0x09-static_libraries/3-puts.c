2d9591e44a45f877b5e032d829ab8606c5dfb889
Kinghope007 <emmanuelhope39@gmail.com>
2023-01-06 06:00:01 -0800

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: input string
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
