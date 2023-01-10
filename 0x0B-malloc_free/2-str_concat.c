a3f9293a98f19de32b61d735332eadd98b82704d
Kinghope007 <emmanuelhope39@gmail.com>
2023-01-10 06:05:11 -0800

    hope

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,8 @@ char *str_concat(char *s1, char *s2)
 {
 	int a = 0;
 	int b = 0;
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 	char  *s;
 
 	if (s1 ==  NULL)
@@ -22,28 +22,28 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 	/** find length of str1  & str2*/
-	while (s1[a] == '\0')
+	while (s1[a])
 		a++;
-	while (s2[b] == '\0')
+	while (s2[b])
 		b++;
 	/* +1 for our end of string character */
-	s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
-
+	s = (char *)malloc(1 * sizeof(char) + 1);
 	if (s == NULL)
 		return (NULL);
 
 	/** add the first string to array s*/
-	for (i = 0; s1[1] != '\0'; i++)
-		s[i] = s1[i];
-	/*add the second string to array s*/
-	for (j = 0; s2[j] != '\0'; j++)
+	b = 0;
+	while (j < i)
 	{
-		s[i] = s2[j];
-		i++;
+		if (j < a)
+			s[j] = s1[j];
+		if (j >= a)
+		{
+			s[j] = s2[j];
+			b++;
+		}
+		j++;
 	}
-
-	/* null terminate our new string*/
-	s[i] = '\0';
-
-	return (s);
+	s[j] = '\0';
+		return (s);
 }
