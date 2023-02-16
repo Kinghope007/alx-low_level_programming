8db62f88c4887936377d86f525c9eedfc71d1333
Kinghope007 <emmanuelhope39@gmail.com>
2023-02-16 03:56:42 -0800

    hope

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return (0).
+ */
+int main(void)
+{
+	print_alphabets_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,18 +8,18 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	int lastdigit;
 
 	if (n < 0)
 	{
-		last_digit = (-1 * (n % 10));
-		_putchar(last_digit + '0');
-		return (last_digit);
+		lastdigit = (-1 * (n % 10));
+		_putchar(lastdigit + '0');
+		return (lastdigit);
 	}
 	else
 	{
-		last_digit = (n % 10);
-		_putchar(last_digit + '0');
-		return (last_digit);
+		lastdigit = (n % 10);
+		_putchar(lastdigit + '0');
+		return (lastdigit);
 	}
 }
