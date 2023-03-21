545957675473fb942ebc3b5b86560993c7af4297
Kinghope007 <emmanuelhope39@gmail.com>
2023-03-21 07:16:47 -0700

    operations '

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: O always
+ */
+int main(void)
+{
+	int a, b;
+	int add, sub, mul, div, mod;
+
+	add = a + b;
+	sub = a - b;
+	mul = a * b;
+	div = a / b;
+	mod = a % b;
+
+	return (0);
+}
